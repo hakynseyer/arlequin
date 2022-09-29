b5777103311debbcaa94d94f65b3ad1078bd70de
Joaquin Reyes Sanchez <hakynseyer@gmail.com>
2022-09-29 01:03:36 -0500

    Arreglando un fallo de PATCH

--- src/Route/Route.php
+++ src/Route/Route.php
@@ -23,8 +23,8 @@ class Route extends Router_Tools {
     }
 
     header('Access-Control-Allow-Headers: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Allow: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH');
+    header('Allow: GET, POST, PUT, DELETE, PATCH');
 
     $baseDir = __DIR__ . '/../../../../../' . $_ENV['BACKEND_ROUTES'];
 
